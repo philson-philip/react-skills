d337ee912057e8bc193962a0aaa074c948f05438
Philson K Philip <philson.k.philip@gmail.com>
2018-08-24 06:37:04 +0530

    Image added
    Added directory and learned to import image from it

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import img1 from './assets/img/img1.jpg';
 
 class App extends Component {
   render() {
@@ -8,11 +9,14 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React Learning</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          React - A JavaScript Library for building the User Interfaces
         </p>
+        <div>
+          <img src={img1} className="Cover" alt="image"/>
+        </div>
       </div>
     );
   }
